210f6dc8d83b38e0ac97f8e27c6dd62f9c659a60
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2022-05-23 20:43:12 +0500

    add new panel

--- src/sanep/components/Yangiliklar/Yangiliklar.jsx
+++ src/sanep/components/Yangiliklar/Yangiliklar.jsx
@@ -1,51 +1,132 @@
-import React, { Component, useEffect } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import './Style/Yangiliklar.scss'
+import axios from "axios";
 const url = 'http://213.230.65.55:10';
-class Yangiliklar extends Component {
-    constructor (props) {
-      super(props)
-        this.state = {
-            posts : []
-        }
-    }   
 
-    // const fetchData = () => {
-    //     fetch(url+"/getPosts", {})
-    //     .then(res=>  {return res.json()})
-    //     .then(data=>{this.setState({posts: data})})
-    //     .catch(e=>{console.log(e)})
-    //     .finally(()=>{
-    //         console.log("posts", this.state.posts)
-    //     })
-    // }
 
-    // useEffect(() => {
-    //     fetchData()
-    // }, [])
+function Yangiliklar() {    
+    const [posts, setPosts] = useState([]);
 
-//  updateAllData(){
-//   fetch(url+"/getPosts", {})
-//   .then(res=>{return res.json()})
-//   .then(data=>{this.setState({posts: data})})
-//   .catch(e=>{console.log(e)})
-//   .finally(()=>{
-//     console.log("posts", this.state.posts)
-//   })
-// }
-   
-    render() {
-        
-        this.updateAllData();
-        // console.log()
-  // const [ openPost, setOpenPost ] = useState({_id: null, caption: null, description: null, imageId: null})
- 
+ const updateAllData =  (function(){
+  axios({
+    url: url + "/posts",
+    method: 'GET',
+    responseType: "json"
+  })
+  .then(data=>{
+      setPosts(()=>{return data.data});
+      
+  })
+  .catch(e=>{
+    console.log(e);
+  })
+})
 
-        return (
-            <div className='yangiliklar-page'>
-                Yangiliklar
-            </div>
-        );
-    }
+  useEffect(updateAllData, [])
+console.log(posts)
+  return (
+      <div className='news-card'>
+          {/* { */}
+              {/* posts.map((post) => { */}
+          {/* return */ }
+                    <div className='news-card__item'>
+                      <div className='news-card__boxImg'>
+                          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5YrQimjeNiD7SwJ0UUV1qUnTO4HPaP-M0GQ&usqp=CAU' alt="images" />
+                      </div>
+                      <div className="context-text">
+                          <h3>Lorem dolor sit bla bla</h3>
+                          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Error neque voluptatibus quibusdam perspiciatis incidunt architecto a quia eveniet et, laudantium rem soluta quaerat beatae. Magnam impedit voluptate dolores hic voluptatibus. </p>
+                          <hr />
+                            <div className='data'>
+                                <span>1.1210.54</span>
+                            </div>
+                      </div>
+                    </div>
+              {/* }) */}
+          {/* } */ }
+          <div className='news-card__item'>
+                      <div  className='news-card__boxImg'>
+                          <img src='https://www.codegrepper.com/codeimages/javascript-set-image-src.png' alt="images" />
+                      </div>
+                      <div className="context-text">
+                          <h3>Lorem dolor sit bla bla</h3>
+                          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit lorem. Error neque voluptatibus quibusdam perspiciatis incidunt architecto a quia eveniet et, laudantium rem soluta quaerat beatae. Magnam impedit voluptate dolores hic voluptatibus. </p>
+                          <hr />
+                  <div className='data'>
+                      <span>1.1210.54</span>
+                          </div>
+                      </div>
+          </div>
+          {/*  */}
+          <div className='news-card__item'>
+                      <div  className='news-card__boxImg'>
+                          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5YrQimjeNiD7SwJ0UUV1qUnTO4HPaP-M0GQ&usqp=CAU' alt="images" />
+                      </div>
+                      <div className="context-text">
+                          <h3>Lorem dolor sit bla bla</h3>
+                          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Error neque voluptatibus quibusdam perspiciatis incidunt architecto a quia eveniet et, laudantium rem soluta quaerat beatae. Magnam impedit voluptate dolores hic voluptatibus. </p>
+                          <hr />
+                  <div className='data'>
+                      <span>1.1210.54</span>
+                  </div>
+                </div>
+          </div>
+           <div className='news-card__item'>
+                      <div  className='news-card__boxImg'>
+                          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5YrQimjeNiD7SwJ0UUV1qUnTO4HPaP-M0GQ&usqp=CAU' alt="images" />
+                      </div>
+                      <div className="context-text">
+                          <h3>Lorem dolor sit bla bla</h3>
+                          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Error neque voluptatibus quibusdam perspiciatis incidunt architecto a quia eveniet et, laudantium rem soluta quaerat beatae. Magnam impedit voluptate dolores hic voluptatibus. </p>
+                          <hr />
+                  <div className='data'>
+                      <span>1.1210.54</span>
+                  </div>
+                </div>
+          </div>
+           <div className='news-card__item'>
+                      <div  className='news-card__boxImg'>
+                          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5YrQimjeNiD7SwJ0UUV1qUnTO4HPaP-M0GQ&usqp=CAU' alt="images" />
+                      </div>
+                      <div className="context-text">
+                          <h3>Lorem dolor sit bla bla</h3>
+                          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Error neque voluptatibus quibusdam perspiciatis incidunt architecto a quia eveniet et, laudantium rem soluta quaerat beatae. Magnam impedit voluptate dolores hic voluptatibus. </p>
+                          <hr />
+                  <div className='data'>
+                      <span>1.1210.54</span>
+                  </div>
+                </div>
+          </div>
+           <div className='news-card__item'>
+                      <div  className='news-card__boxImg'>
+                          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5YrQimjeNiD7SwJ0UUV1qUnTO4HPaP-M0GQ&usqp=CAU' alt="images" />
+                      </div>
+                      <div className="context-text">
+                          <h3>Lorem dolor sit bla bla</h3>
+                          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Error neque voluptatibus quibusdam perspiciatis incidunt architecto a quia eveniet et, laudantium rem soluta quaerat beatae. Magnam impedit voluptate dolores hic voluptatibus. </p>
+                          <hr />
+                  <div className='data'>
+                      <span>1.1210.54</span>
+                  </div>
+                </div>
+          </div>
+           <div className='news-card__item'>
+                      <div  className='news-card__boxImg'>
+                          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5YrQimjeNiD7SwJ0UUV1qUnTO4HPaP-M0GQ&usqp=CAU' alt="images" />
+                      </div>
+                      <div className="context-text">
+                          <h3>Lorem dolor sit bla bla</h3>
+                          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Error neque voluptatibus quibusdam perspiciatis incidunt architecto a quia eveniet et, laudantium rem soluta quaerat beatae. Magnam impedit voluptate dolores hic voluptatibus. </p>
+                          <hr />
+                  <div className='data'>
+                      <span>1.1210.54</span>
+                  </div>
+                </div>
+          </div>
+    </div>
+  )
 }
 
-export default Yangiliklar;
+export default Yangiliklar
+
+
